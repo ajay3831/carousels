6076d8505a740c024563adf27997dc5046691a28
Ajay <ajayc@qburst.com>
2019-12-26 16:24:33 +0530

    Refactoring the application Included responsiveness using flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,130 +1,83 @@
-import { Component, ViewChild } from '@angular/core';
-import { DisplayCardComponent } from './display-card/display-card.component';
+import { Component } from '@angular/core';
 
+import {cardDetails} from './mocks/mock-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
 
-
   /**
-   * To adjust the carousel position based on the slider input
+   * To adjust carousel position;
    * @type {number}
    * @memberof AppComponent
    */
-  public leftPosition : number;
+  public leftPosition: number = 0;
 
   /**
-   * Title of the application
-   * @type {number}
+   * To set the transition effect while the slide moves
+   * @type {string}
    * @memberof AppComponent
    */
-  public title = 'carousel';
+  public transitionEffect: string = 'unset'
 
   /**
-   * To get the slide control position
+   * Data for the card details
    * @memberof AppComponent
    */
-  public slideControl = 0;
+  public cardDetails = cardDetails;
 
-  /**
-   * Detail to be displayed in card format
-   * @memberof AppComponent
-   */
-  public cardDetails = [
-    {
-      avatar: '',
-      name: 'Zoro',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Sanji',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Luffy',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Brook',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Ace',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Robin',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Ussop',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Chopper',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Marco',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Kadio',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Franky',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-    {
-      avatar: '',
-      name: 'Merry',
-      descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
-    },
-  ]
-
-
-  constructor() {
-    this.leftPosition = 300;
-   }
-
-  /**
-   * Function to go to previous slide
-   * @memberof AppComponent
-   */
-  public prev() : void{
-    
-    if(this.slideControl >0) {
-      this.leftPosition += 350;
-      this.slideControl = this.slideControl-1
-    }
+  ngOnInit() {
+  //  this.startCarousel();
   }
-  
+
   /**
-   * Function to go to next slide
+   * Function to move to previous slide
+   * @returns {void}
    * @memberof AppComponent
    */
-  public next() : void {
-
-    if(this.slideControl < this.cardDetails.length-1) {
-      this.leftPosition -= 350    
-      this.slideControl = this.slideControl+1
-    }
+  public prev() : void {
+    this.transitionEffect = 'left 2s';
+    this.leftPosition = this.leftPosition + 100;
   }
+
+  /**
+   * Function to move to next slide
+   * @returns {void}
+   * @memberof AppComponent
+   */
+  public next(): void {
+    this.transitionEffect = 'left 2s';
+    this.leftPosition = this.leftPosition - 100;
+  }
+
+  /**
+   * Function to move the slide to move the slide at specific slide
+   * @param {number} index
+   * @memberof AppComponent
+   */
+  public moveToSlideAt(index : number): void { 
+    this.transitionEffect = 'unset';
+    this.leftPosition = index * -100;
+  }
+
+  /**
+   * To run the slide show
+   * @returns {void}
+   * @memberof AppComponent
+   */
+  public startCarousel(): void {
+    setInterval(() => {
+      if(this.leftPosition > (-100 * (this.cardDetails.length-1))) {
+        this.next();
+      } else {
+        this.transitionEffect = 'unset';
+        this.leftPosition = 0;
+      }
+    },3000)
+  }
+
 
 }
---
+++ src/app/mocks/mock-data.ts
@@ -0,0 +1,62 @@
+export const cardDetails = [
+    {
+      avatar: '',
+      name: 'Zoro',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Sanji',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Luffy',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Brook',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Ace',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Robin',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Ussop',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Chopper',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Marco',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Kadio',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Franky',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+    {
+      avatar: '',
+      name: 'Merry',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem id enim. Ducimus quisquam qui cum quis voluptatum non asperiores? Molestias facere dignissimos sed repellendus. Vitae distinctio consequatur officia, dolorem quia corporis quaerat libero rerum? Mollitia consequatur laudantium delectus modi?'
+    },
+  ]
